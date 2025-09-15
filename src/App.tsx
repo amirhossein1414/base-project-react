681ce5d5f0995be1b4b17fa2431951bad6d0f647
a.rezaei <a.rezaei@sadad.co.ir>
2025-09-15 15:31:38 +0430

    adding-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+/* 
 import './App.css';
 
 function App() {
@@ -11,4 +11,35 @@ function App() {
   )
 }
 
-export default App
+export default App */
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        {/* Top Bar */}
+        <header className="top-bar">
+          <img src="/menu/top.png" alt="Top bar" className="top-bar-img" />
+        </header>
+
+        {/* Main area with side menu (right) and routed content (center) */}
+        <main className="content-area">
+          <aside className="side-menu">
+            <img src="/menu/side.png" alt="Side menu" className="side-menu-img" />
+          </aside>
+
+          <section className="page-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            {/* Add more routes here */}
+            </Routes>
+          </section>
+        </main>
+      </div>
+    </Router>
+  );
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <form className="my-form">
+            <h1>فرم نمونه</h1>
+            <label>
+                نام:
+                <input type="text" />
+            </label>
+        </form>
+    );
+}
